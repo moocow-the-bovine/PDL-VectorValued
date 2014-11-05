ccb47bc8e85f1b12d737b1ebf20ae0a18d78c49e
Bryan Jurish <jurish@bbaw.de>
2014-11-05 12:17:24 +0000

    * v0.08: improved handling of empty pdls
    git-svn-id: file:///home/moocow/svn/public/diss-perl/trunk/PDL-VectorValued@2299 097f6656-c427-11dc-a790-1b5d5187385a

--- VectorValued/Version.pm
+++ VectorValued/Version.pm
@@ -7,7 +7,7 @@
 ##======================================================================
 
 package PDL::VectorValued::Version;
-our $VERSION = '0.07002';
+our $VERSION = '0.08';
 $PDL::VectorValued::VERSION = $VERSION;
 
 1; ##-- make perl happy
