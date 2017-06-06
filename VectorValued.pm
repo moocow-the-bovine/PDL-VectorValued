2277fd1481044b4ec88df3a2f7690545e05af598
Bryan Jurish <jurish@bbaw.de>
2017-06-06 08:09:55 +0000

    PDL-VectorValued v1.0.7: added 'use lib .' to Makefile.PL   - hopefully fix RT bug #121661
    git-svn-id: file:///home/moocow/svn/public/diss-perl/trunk/PDL-VectorValued@2470 097f6656-c427-11dc-a790-1b5d5187385a

--- VectorValued.pm
+++ VectorValued.pm
@@ -27,7 +27,7 @@ our %EXPORT_TAGS =
   );
 
 ## VERSION was formerly set by PDL::VectorValued::Version, now use perl-reversion from Perl::Version instead
-our $VERSION = '1.0.6';
+our $VERSION = '1.0.7';
 
 ##======================================================================
 ## pod: header
