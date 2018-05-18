1063c1d3997966670ccd1fb8e807447c90a7da80
Bryan Jurish <jurish@bbaw.de>
2018-05-18 07:47:27 +0000

    + readme.rpod MANIFEST inclusion order
    git-svn-id: file:///home/moocow/svn/public/diss-perl/trunk/PDL-VectorValued@2515 097f6656-c427-11dc-a790-1b5d5187385a

--- VectorValued.pm
+++ VectorValued.pm
@@ -27,7 +27,7 @@ our %EXPORT_TAGS =
   );
 
 ## VERSION was formerly set by PDL::VectorValued::Version, now use perl-reversion from Perl::Version instead
-our $VERSION = '1.0.8';
+our $VERSION = '1.0.9';
 
 ##======================================================================
 ## pod: header
