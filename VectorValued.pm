c0912049db55bc801b382e4eb24e6f8dd76341c5
Bryan Jurish <moocow.bovine@gmail.com>
2024-06-16 17:08:06 +0200

    bump to v1.0.23

--- VectorValued.pm
+++ VectorValued.pm
@@ -80,7 +80,7 @@ our %EXPORT_TAGS =
   );
 
 ## VERSION was formerly set by PDL::VectorValued::Version, now use perl-reversion from Perl::Version instead
-our $VERSION = '1.0.22';
+our $VERSION = '1.0.23';
 
 ##======================================================================
 ## pod: header
